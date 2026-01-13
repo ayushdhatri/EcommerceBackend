ca8ab650d4fa5490ca8379763509a177c6b1711c
Ayush Solanki <aayushdhatri@gmail.com>
2026-01-14 01:15:13 +0530

    Adding Category backend

---
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -0,0 +1,21 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService{
+    private List<Category> categories = new ArrayList<>();
+    @Override
+    public List<Category> getAllCategoreis() {
+       return categories;
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        this.categories.add(category);
+    }
+}
