8220ef26862b52766f53f4dfcfe7f82fd454b25f
Ayush Solanki <aayushdhatri@gmail.com>
2026-01-17 00:59:57 +0530

    Added sorting logic

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.controller;
 
+import com.ecommerce.project.config.AppConstants;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
@@ -26,10 +27,13 @@ public class CategoryController {
 
     @GetMapping("/api/public/categories")
     public ResponseEntity<CategoryResponse> getAllCategories(
-            @RequestParam(name = "pageNumber")Integer pageNumber,
-            @RequestParam(name = "pageSize") Integer pageSize)
+            @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false)Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_CATEGORIES_BY, required = false) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR, required = false) String sortOrder)
     {
-        CategoryResponse categoryResponse = categoryService.getAllCategoreis(pageNumber,pageSize);
+
+        CategoryResponse categoryResponse = categoryService.getAllCategoreis(pageNumber,pageSize, sortBy, sortOrder);
         return new ResponseEntity<>(categoryResponse,HttpStatus.OK);
 
     }
--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Service
 public interface CategoryService {
-    CategoryResponse getAllCategoreis(Integer pageNumber, Integer pageSize);
+    CategoryResponse getAllCategoreis(Integer pageNumber, Integer pageSize,String sortBy, String sortOrder);
     CategoryDTO createCategory(CategoryDTO categoryDTO);
     CategoryDTO deleteCategory(Long id);
     CategoryDTO updateCategory(CategoryDTO category, Long id);
