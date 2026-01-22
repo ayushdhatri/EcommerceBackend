5fa561ba21157937e1d6a03528b9dea61379847b
Ayush Solanki <aayushdhatri@gmail.com>
2026-01-23 01:22:26 +0530

    Adding modle user, address, approle, role

---
+++ src/main/java/com/ecommerce/project/model/Role.java
@@ -0,0 +1,38 @@
+package com.ecommerce.project.model;
+import jakarta.persistence.*;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20,name = "role_name")
+    private AppRole roleName;
+
+    public Role(AppRole roleName) {
+        this.roleName = roleName;
+    }
+
+    public Role() {
+
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public AppRole getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(AppRole roleName) {
+        this.roleName = roleName;
+    }
+}
---
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -0,0 +1,67 @@
+package com.ecommerce.project.model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    private String userName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    public User(){
+
+    }
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
