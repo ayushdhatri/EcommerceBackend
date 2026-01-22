3d131b724ef595064693a516a4aa721c13ad6047
Ayush Solanki <aayushdhatri@gmail.com>
2026-01-23 02:46:55 +0530

    Establishing relationship between user and address

--- src/main/java/com/ecommerce/project/model/Address.java
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -1,15 +1,44 @@
 package com.ecommerce.project.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name = "addresses")
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long addressId;
 
+    @NotBlank
+    @Size(min = 5, message = "Steet name must be atleast 5 character")
+    private String street;
+
+    @NotBlank
+    @Size(min = 5, message = "Building name must be atleast 5 character")
+    private String buildingName;
+
+    @NotBlank
+    @Size(min = 2, message = "Address name must be atleast 2 character")
+    private String city;
+
+    @NotBlank
+    @Size(min = 4, message = "State name must be atleast 4 character")
+    private String state;
+
+    @NotBlank
+    @Size(min = 2, message = "Country name must be atleast 2 character")
+    private String country;
+
+    @NotBlank
+    @Size(min = 6, message = "Pincode name must be atleast 6 character")
+    private String pincode;
+
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users = new ArrayList<>();
 
 }
--- src/main/java/com/ecommerce/project/model/User.java
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -4,7 +4,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -41,6 +43,12 @@ public class User {
                 cascade = {CascadeType.MERGE,CascadeType.PERSIST},
                 orphanRemoval = true)
     private Set<Product> products;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    @JoinTable(name = "user_address",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "address_id"))
+    private List<Address> addresses = new ArrayList<>();
 
 
     public User(){
