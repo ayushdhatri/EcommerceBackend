2115ff57367f4e4befc25ccbabe5c880b7fe2c74
Ayush Solanki <aayushdhatri@gmail.com>
2026-02-04 19:05:25 +0530

    Adding address basic repository and service class

---
+++ src/main/java/com/ecommerce/project/controller/AddressController.java
@@ -0,0 +1,19 @@
+package com.ecommerce.project.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class AddressController {
+
+
+
+
+
+
+
+
+
+}
