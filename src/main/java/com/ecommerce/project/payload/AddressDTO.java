ca180e5159be28a17e750e949a933e573c4fa19f
Ayush Solanki <aayushdhatri@gmail.com>
2026-02-04 19:09:08 +0530

    Adding Address DTO

---
+++ src/main/java/com/ecommerce/project/payload/AddressDTO.java
@@ -0,0 +1,58 @@
+package com.ecommerce.project.payload;
+
+public class AddressDTO {
+    private Long addressId;
+    private String street;
+    private String buildingName;
+    private String city;
+    private String state;
+    private String country;
+
+    public Long getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Long addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getBuildingName() {
+        return buildingName;
+    }
+
+    public void setBuildingName(String buildingName) {
+        this.buildingName = buildingName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
